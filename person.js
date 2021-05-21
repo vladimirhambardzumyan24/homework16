438a6965bd456dcf30c6e1f60c046459e50329f0
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-05-21 15:57:12 +0400

    Person Age

---
+++ person.js
@@ -0,0 +1,23 @@
+function Person(name, age) {
+  let person = {
+    name,
+    age,
+    compareAge(obj) {
+      if (this.age < obj.age) {
+        return `${this.name} is younger than me.`;
+      } else if (this.age > obj.age) {
+        return `${this.name} is older than me.`;
+      } else {
+        return `${this.name} is the same age as me.`;
+      }
+    },
+  };
+  return person;
+}
+
+const p1 = Person("Samuel", 24);
+const p2 = Person("Joel", 36);
+const p3 = Person("Lily", 24);
+console.log(p1.compareAge(p2));
+console.log(p2.compareAge(p1));
+console.log(p1.compareAge(p3));
